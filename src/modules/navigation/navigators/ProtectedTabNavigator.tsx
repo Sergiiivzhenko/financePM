e21ce1488ba42bb8276a0f511f569b8c8faae65a
Sergey Ivzhenko <sergey.ivzhenko@eosda.com>
2020-03-28 01:00:29 +0200

    added nav icons

--- src/modules/navigation/navigators/ProtectedTabNavigator.tsx
+++ src/modules/navigation/navigators/ProtectedTabNavigator.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import { Ionicons } from '@expo/vector-icons';
 import {HomeScreen} from "../../home/screens/HomeScreen";
 import {TransfersScreen} from "../../transfers/screens/TransfersScreen";
 import {ReportsScreen} from "../../reports/screens/ReportsScreen";
@@ -7,8 +8,30 @@ import {SettingsScreen} from "../../settings/screens/SettingsScreen";
 
 const Tab = createBottomTabNavigator();
 
+const getIcon = name => {
+    switch (name) {
+        case 'Home': {
+            return 'ios-card';
+        }
+        case 'Transfers': {
+            return 'ios-swap';
+        }
+        case 'Reports': {
+            return 'ios-stats';
+        }
+        case 'Settings': {
+            return 'ios-settings';
+        }
+        default: return '';
+    }
+};
+
 export const ProtectedTabNavigator = () => (
-    <Tab.Navigator>
+    <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarIcon: () => <Ionicons name={getIcon(route.name)} size={24} />,
+        })}
+        >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Transfers" component={TransfersScreen} />
         <Tab.Screen name="Reports" component={ReportsScreen} />
