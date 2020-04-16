0af2f84648c7805cf2c18c2d0b9a83485f0251ec
Sergey Ivzhenko <sergey.ivzhenko@eosda.com>
2020-04-16 14:35:29 +0300

    setup icons selection, refactored form, added useCallback hooks

---
+++ src/modules/transfers/utils/accountIcons.ts
@@ -0,0 +1,15 @@
+import CardBlack from '../../../../assets/card-black.jpg';
+import CardBlue from '../../../../assets/card-blue.jpg';
+import CardWhite from '../../../../assets/card-white.jpg';
+import WalletMan from '../../../../assets/man-wallet.jpg';
+import WalletWoman from '../../../../assets/woman-wallet.jpg';
+import WalletBrown from '../../../../assets/wallet-brown.jpg';
+
+export const accountIcons = [
+    CardBlack,
+    CardBlue,
+    CardWhite,
+    WalletMan,
+    WalletWoman,
+    WalletBrown,
+];
