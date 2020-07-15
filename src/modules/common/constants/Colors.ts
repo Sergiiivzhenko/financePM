d14bde3c02110b7af4264623a242538a57f54582
Sergey Ivzhenko <sergey.ivzhenko@eosda.com>
2020-07-15 21:53:40 +0300

    setup svg charts, added test chart

--- src/modules/common/constants/Colors.ts
+++ src/modules/common/constants/Colors.ts
@@ -1,4 +1,6 @@
 export const Colors = {
     blue: '#1976d2',
     gray: 'gray',
+    red: '#DC143C',
+    green: '#3CB371'
 };
--- src/modules/reports/screens/IncomeOutcomeReportScreen.tsx
+++ src/modules/reports/screens/IncomeOutcomeReportScreen.tsx
@@ -1,8 +1,25 @@
 import React from "react";
-import {Text, View} from "react-native";
+import {View} from "react-native";
+import {PieChart} from 'react-native-svg-charts'
+import {Colors} from "../../common/constants/Colors";
+import {CATEGORY} from "../../settings/utils/category.enum";
 
-export const IncomeOutcomeReportScreen = () => (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>IncomeOutcomeReportScreen</Text>
-    </View>
-);
+export const IncomeOutcomeReportScreen = () => {
+    const data = [
+        {
+            value: 60,
+            svg: {fill: Colors.green},
+            key: CATEGORY.INCOME,
+        },
+        {
+            value: 40,
+            svg: {fill: Colors.red},
+            key: CATEGORY.OUTCOME,
+        },
+    ]
+    return (
+        <View>
+            <PieChart style={{ height: 200 }} data={data} />
+        </View>
+    );
+}
