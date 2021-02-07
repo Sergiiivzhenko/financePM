d0250f6308878f7dee1ef71b760b4386663a842e
sergiiivzhenko <sergiiivzhenko@gmail.com>
2021-02-07 22:17:13 +0200

    updated home screen button

--- src/modules/home/screens/HomeScreen.tsx
+++ src/modules/home/screens/HomeScreen.tsx
@@ -1,21 +1,23 @@
 import React from "react";
-import {StyleSheet, ScrollView, View} from "react-native";
-import {Button, Text} from "native-base";
+import {StyleSheet, ScrollView, View, TouchableOpacity} from "react-native";
 import {connect} from 'react-redux';
 import Constants from 'expo-constants';
 import {HomeAccountCard} from "../components/HomeAccountCard";
 import {getAccountsWithActualBalance} from "../../common/utils/getAccountsWithActualBalance";
+import { AntDesign } from "@expo/vector-icons";
 
 const HomeScreenComponent = ({navigation, accounts, transactions}) => {
     const onAddAccountHandler = () => navigation.navigate('Transfers', {screen: 'AddAccount'});
     const accountsWithActualBalance = getAccountsWithActualBalance(accounts, transactions);
     return (
         <ScrollView style={styles.container}>
-            <Button style={styles.addAccountButton} onPress={onAddAccountHandler}>
-                <Text>Add Account</Text>
-            </Button>
             <View style={styles.list}>
                 {accountsWithActualBalance.map(item => <HomeAccountCard key={item.id} item={item} />)}
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={onAddAccountHandler}>
+                    <AntDesign name="plus" size={60} color="#2C45C6" style={styles.button} />
+                </TouchableOpacity>
             </View>
         </ScrollView>
     );
@@ -32,7 +34,15 @@ const styles = StyleSheet.create({
         flex: 1,
         flexWrap: 'wrap',
     },
-
+    buttonContainer: {
+        marginTop: 20,
+        alignItems: 'center',
+    },
+    button: {
+        borderRadius: 11,
+        borderWidth: 1,
+        borderColor: 'rgba(44, 69, 198, 0.3)',
+    },
 });
 
 const mapStateToProps = (state) => {
