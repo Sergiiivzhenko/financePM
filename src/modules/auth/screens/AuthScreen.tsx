724e6493913988399d7ca843d0a80292b0f05b11
sergiiivzhenko <sergiiivzhenko@gmail.com>
2021-02-07 23:36:40 +0200

    updated sing in/up screen

--- src/modules/auth/screens/AuthScreen.tsx
+++ src/modules/auth/screens/AuthScreen.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
-import {View, StyleSheet, Keyboard} from "react-native";
-import {Container, Button, Text, Item, Input, Label} from 'native-base';
+import {View, StyleSheet, Keyboard, TouchableOpacity} from "react-native";
+import {Container, Button, Text, Item, Input} from 'native-base';
+import Constants from "expo-constants";
 import {connect} from 'react-redux';
 import {Base64} from 'js-base64';
 import {isEmpty} from 'lodash';
@@ -11,6 +12,7 @@ import {inputValidate} from "../utils/inputValidate";
 const AuthScreenComponent = ({navigation, currentUser, login, register, error, setError}) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
     const [isSignIn, setIsSignIn] = useState(true);
 
     if (!isEmpty(currentUser)) {
@@ -25,54 +27,112 @@ const AuthScreenComponent = ({navigation, currentUser, login, register, error, s
         Keyboard.dismiss();
         const emailValid = inputValidate('email', email, setError);
         const passwordValid = inputValidate('password', password, setError);
-        if (emailValid && passwordValid) {
+        let repeatPasswordValid = true;
+        let arePasswordsEqual = true;
+        if (!isSignIn) {
+            repeatPasswordValid = inputValidate('password', repeatPassword, setError);
+            arePasswordsEqual = password === repeatPassword;
+            if (!arePasswordsEqual) {
+                setError('Passwords are not equal');
+            }
+        }
+        if (emailValid && passwordValid && repeatPasswordValid && arePasswordsEqual) {
             action({email, password: Base64.decode(password)});
         }
     };
 
     return (
-        <Container style={styles.container}>
-            <View style={styles.margin}>
-                <Item style={styles.marginBottom} floatingLabel>
-                    <Label style={styles.padding}>Email</Label>
-                    <Input style={styles.padding} value={email} onChangeText={setEmail}/>
-                </Item>
-                <Item style={styles.marginBottom} floatingLabel>
-                    <Label style={styles.padding}>Password</Label>
-                    <Input
-                        style={styles.padding}
-                        value={password}
-                        onChangeText={setPassword}
-                        secureTextEntry
-                    />
-                </Item>
-                {error && <Text style={styles.error}>{error}</Text>}
+        <View style={styles.mainContainer}>
+            <View style={styles.header}>
+                <TouchableOpacity onPress={() => setIsSignIn(!isSignIn)} style={styles.leftNavigationContainer}>
+                    <Text style={styles.leftNavigation}>{switchSignInText}</Text>
+                </TouchableOpacity>
+                <Text style={styles.title}>{actionButtonText}</Text>
             </View>
-            <Button
-                style={[styles.button, styles.margin]}
-                onPress={onPress}
-            >
-                <Text>{actionButtonText}</Text>
-            </Button>
-            <View style={styles.authSelection}>
-                <Text>or </Text>
-                <Text uppercase style={styles.authSelectionText} onPress={() => setIsSignIn(!isSignIn)}>
-                    {switchSignInText}
-                </Text>
-            </View>
-        </Container>
+            <Container style={styles.container}>
+                <View style={styles.margin}>
+                    <Text style={styles.label}>Email</Text>
+                    <Item style={styles.inputContainer} rounded>
+                        <Input style={styles.padding} value={email} onChangeText={setEmail}/>
+                    </Item>
+                    <Text style={styles.label}>Password</Text>
+                    <Item style={styles.inputContainer} rounded>
+                        <Input
+                            style={styles.padding}
+                            value={password}
+                            onChangeText={setPassword}
+                            secureTextEntry
+                        />
+                    </Item>
+                    {!isSignIn && (
+                        <>
+                        <Text style={styles.label}>Repeat password</Text>
+                        <Item style={styles.inputContainer} rounded>
+                            <Input
+                                style={styles.padding}
+                                value={repeatPassword}
+                                onChangeText={setRepeatPassword}
+                                secureTextEntry
+                            />
+                        </Item>
+                        </>
+                    )}
+                    {error && <Text style={styles.error}>{error}</Text>}
+                </View>
+                <Button
+                    style={[styles.button, styles.margin]}
+                    onPress={onPress}
+                >
+                    <Text>{actionButtonText}</Text>
+                </Button>
+            </Container>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
+    inputContainer: {
+        borderWidth: 2,
+        borderColor: '#969BBA',
+        borderRadius: 10,
+        marginBottom: 20,
+    },
+    label: {
+        paddingLeft: 5,
+        fontSize: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    leftNavigationContainer: {
+        position: 'absolute',
+        left: 10,
+    },
+    leftNavigation: {
+        color: Colors.blue,
+        fontSize: 18,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    mainContainer: {
+        flex: 1,
+        paddingTop: Constants.statusBarHeight + 10,
+        backgroundColor: 'white',
+    },
     container: {
         justifyContent: 'center',
     },
     button: {
         justifyContent: 'center',
         marginLeft: 20,
+        marginTop: 20,
         paddingVertical: 30,
         backgroundColor: Colors.blue,
+        borderRadius: 10,
     },
     marginBottom: {
         marginBottom: 10,
